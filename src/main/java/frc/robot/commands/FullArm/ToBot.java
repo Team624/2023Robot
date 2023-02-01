2a8fc701816d93f43dea8ec23285db3690247f95
Arnav Joshi <74690929+arnav-joshi@users.noreply.github.com>
2023-01-31 20:50:39 -0600

    added commands, add goal encoder positions later

---
+++ src/main/java/frc/robot/commands/FullArm/ToBot.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.FullArm;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Pivot;
+
+
+public class ToBot extends CommandBase {
+  private final Pivot pivot;
+  private final Arm arm;
+  /** Creates a new ToLowGoal. */
+  public ToBot(Pivot pivot, Arm arm) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.pivot=pivot;
+    this.arm=arm;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    pivot.pivotTo(0);
+    arm.extendTo(0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    pivot.stop();
+    arm.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
