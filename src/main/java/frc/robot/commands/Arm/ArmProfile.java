cecd3040886a8789915633489e3efd1e50efce27
kartikr27 <krishsuper27@gmail.com>
2023-02-21 08:37:36 -0600

    updates

--- src/main/java/frc/robot/commands/Arm/ArmProfile.java
+++ src/main/java/frc/robot/commands/Arm/ArmProfile.java
@@ -11,19 +11,22 @@ import frc.robot.subsystems.Arm;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ArmSetpoint extends TrapezoidProfileCommand {
-  /** Creates a new ArmSetpoint. */
-  public ArmSetpoint(Arm m_arm, double setpoint) {
+public class ArmProfile extends TrapezoidProfileCommand {
+  /** Creates a new ArmProfile. */
+  public ArmProfile(Arm m_Arm, double setPoint) {
     super(
+        // The motion profile to be executed
         new TrapezoidProfile(
-            // Limit the max acceleration and velocity
-            new TrapezoidProfile.Constraints(1, 3),
-            // End at desired position in meters; implicitly starts at 0
-            new TrapezoidProfile.State(setpoint, 0)),
-        // Pipe the profile state to the drive
-        setpointState -> m_arm.ArmProfile(setpointState),
-        // Require the drive
-        m_arm);
-    System.out.println(m_arm.getBoreEncoder());
+            // The motion profile constraints
+            new TrapezoidProfile.Constraints(1, 1.1),
+            // Goal state
+            new TrapezoidProfile.State(setPoint,0),
+            // Initial state
+            new TrapezoidProfile.State(m_Arm.getBoreEncoder(),0)),
+        state -> {
+          // Use current trajectory state here
+          m_Arm.ArmProfile(state);
+        },
+        m_Arm);
   }
 }
