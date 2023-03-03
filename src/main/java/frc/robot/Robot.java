c009d036e3d9a53b5c6d11214f7cd68f0ba91c40
Eli Nicksic <elinicksic@gmail.com>
2023-03-02 23:44:47 -0600

    Remove the compressor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,8 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -26,8 +24,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-  private Compressor compressor;
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -42,8 +38,6 @@ public class Robot extends TimedRobot {
 
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-
-    compressor = new Compressor(PneumaticsModuleType.CTREPCM);
 
     m_robotContainer = new RobotContainer();
 
@@ -72,7 +66,6 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledInit() {
     m_robotContainer.ghostSwerve();
-    compressor.disable();
 
     if (m_autonomousCommand != null) m_autonomousCommand.cancel();
   }
@@ -84,8 +77,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     // schedule the autonomous command (example)
-
-    // compressor.enableDigital();
 
     m_robotContainer.setDisabledDrivetrainDefault();
     this.m_autonomousCommand = m_robotContainer.getAutonManager();
@@ -103,8 +94,6 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-
-    // compressor.enableDigital();
 
     m_robotContainer.setDrivetrainDefaultCommand();
   }
